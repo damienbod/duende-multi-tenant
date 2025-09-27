6d12bcb16fd99cbf980641fc0eb0089c5e5783ef
damienbod <damien_bod@hotmail.com>
2025-09-27 07:44:27 +0200

    Update packages

--- src/ShopClientUI/Program.cs
+++ src/ShopClientUI/Program.cs
@@ -1,4 +1,5 @@
 using AdminClientUI;
+using Duende.AccessTokenManagement.OpenIdConnect;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
